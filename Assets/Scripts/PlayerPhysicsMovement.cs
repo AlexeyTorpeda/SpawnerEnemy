89e214ef9425d011a3be66f8e17f44596fe02ba9
AlexeyTorpeda <103385895+AlexeyTorpeda@users.noreply.github.com>
2023-04-08 13:18:16 +0300

    Changes

--- Assets/Scripts/PlayerPhysicsMovement.cs
+++ Assets/Scripts/PlayerPhysicsMovement.cs
@@ -61,11 +61,11 @@ public class PlayerPhysicsMovement : MonoBehaviour
         Vector2 moveAlongGround = new Vector2(_groundNormal.y, -_groundNormal.x);
         Vector2 move = moveAlongGround * deltaPosition.x;
 
-        Movement(move, false);
+        DoMovement(move, false);
 
         move = Vector2.up * deltaPosition.y;
 
-        Movement(move, true);
+        DoMovement(move, true);
 
         if (_horizontalMove < 0 && _isFacingRight)
         {
@@ -77,7 +77,7 @@ public class PlayerPhysicsMovement : MonoBehaviour
         }
     }
 
-    private void Movement(Vector2 move, bool yMovement)
+    private void DoMovement(Vector2 move, bool yMovement)
     {
         float distance = move.magnitude;
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private BlopBot _blopBot;
+    [SerializeField] private BlopBot _prefab;
     [SerializeField] private Vector2 _firstPosition;
+    [SerializeField] private Vector2 _spawnRange = new Vector2(-6, 6);
     [SerializeField] private float _interval;
 
-    private float _minPosition = -6;
-    private float _maxPosition = 6;
     private int _countOfEnemies = 5;
 
     private void Start()
@@ -24,8 +23,8 @@ public class Spawner : MonoBehaviour
 
         for (int i = 0; i < _countOfEnemies; i++)
         {
-            Instantiate(_blopBot, _firstPosition, Quaternion.identity);
-            _firstPosition.x += Random.Range(_minPosition, _maxPosition);
+            Instantiate(_prefab, _firstPosition, Quaternion.identity);
+            _firstPosition.x += Random.Range(_spawnRange.x, _spawnRange.y);
             yield return waitForIntervalSeconds;
         }
     }
